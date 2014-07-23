53c2854ee478e02f0fae38391e69aea61d83251f
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2014-07-23 10:24:04 +0100

    Added Content Model

---
+++ models/Content.js
@@ -0,0 +1,16 @@
+var util = require("util");
+var achilles = require("achilles");
+
+function Content(type, data) {
+	achilles.Model.call(this);
+
+	this.define("type", String); // "rich", "latex"
+	this.define("data", String);
+
+	this.type = type;
+	this.data = data;
+}
+
+util.inherits(Content, achilles.Model);
+
+module.exports = Content;
