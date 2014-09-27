8c70395f9fbd8f19b5fe5855b5127eb9da4ce536
BillsJ <josephbills7@gmail.com>
2014-09-27 11:59:52 +0100

    Update Quiz.js

--- models/Quiz.js
+++ models/Quiz.js
@@ -16,7 +16,7 @@ function Question() {
 
 	this.answer_type = "text";
 	this.on("change:answer_type", this.delAnswer.bind(this));
-};
+}
 
 Question.prototype.delAnswer = function() {
 	this.answer_text = null;
