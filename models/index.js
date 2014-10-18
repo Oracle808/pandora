2aafedc57e89747ef8e6fb0abd6d0eb897705ca3
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2014-10-18 11:13:45 +0100

    Added support for creating vocab quizzes

--- models/index.js
+++ models/index.js
@@ -3,7 +3,8 @@ var models = {};
 models.Content = require("./Content");
 models.Post = require("./Post");
 models.Quiz = require("./Quiz");
-models.VocabQuiz = require("./VocabQuiz");
+models.VocabQuiz = require("./VocabQuiz").VocabQuiz;
+models.VocabQuestion = require("./VocabQuiz").VocabQuestion;
 models.Course = require("./Course");
 models.Club = require("./Club");
 
