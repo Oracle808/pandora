40388f100d6c5d80eb8c1247702a88fd21cfaad1
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2014-10-19 20:04:04 +0100

    Updated quiz structure

--- models/Quiz.js
+++ models/Quiz.js
@@ -2,6 +2,29 @@ var util = require("util");
 var achilles = require("achilles");
 var Content = require("./Content");
 
+function QuestionAttempt() {
+	achilles.Model.call(this);
+
+	this.define("question", String); // refers to question
+	this.define("answer_type", String); // "text", "number", "radio", "checkbox"
+	this.define("answer_text", String);
+	this.define("answer_number", Number);
+	this.define("answer_radio", String);
+	this.define("answer_checkbox", [String]);
+}
+
+util.inherits(QuestionAttempt, achilles.Model);
+
+
+function QuizAttempt() {
+	achilles.Model.call(this);
+
+	this.ref("user", achilles.User);
+	this.define("question", [QuestionAttempt]);
+}
+
+util.inherits(QuizAttempt, achilles.Model);
+
 function Question() {
 	achilles.Model.call(this);
 
@@ -33,10 +56,11 @@ Question.prototype.delAnswer = function() {
 
 function Quiz() {
 	achilles.Model.call(this);
-	
+
 	this.define("questions", [Question]);
 	this.define("title", String);
 	this.define("randomise_questions", Boolean);
+	this.define("attempts", [QuizAttempt]);
 
 	this.questions = [];
 }
